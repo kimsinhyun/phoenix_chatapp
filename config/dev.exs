6ce5f4f52ef5fd3a0a2937fafd5f499c5303e293
sinhyun Kim <ksh415622@naver.com>
2025-06-14 23:32:30 +0900

    db setting

--- config/dev.exs
+++ config/dev.exs
@@ -2,10 +2,11 @@ import Config
 
 # Configure your database
 config :chatapp, Chatapp.Repo,
-  username: System.get_env("DB_USERNAME") || "postgres",
-  password: System.get_env("DB_PASSWORD") || "postgres",
+  username: System.get_env("DB_USERNAME") || "mori",
+  password: System.get_env("DB_PASSWORD") || "mori",
   hostname: System.get_env("DB_HOSTNAME") || "localhost",
   database: System.get_env("DB_NAME") || "chatapp_dev",
+  port: String.to_integer(System.get_env("DB_PORT") || "5436"),
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
