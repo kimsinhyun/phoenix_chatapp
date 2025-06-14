ca927fe59327f979b78b8c2c5f4dfc2eeccf2f2b
sinhyun Kim <ksh415622@naver.com>
2025-06-14 23:44:34 +0900

    rails 의 devise 랑의 차이를 느끼자면, magic 이 아니라, 완벽하게 code generator 로써의 역할만 하는게 너무너무너무 좋은듯. ( mix phx.gen.auth Accounts User users 한 결과                                                                                        )

--- lib/chatapp_web/router.ex
+++ lib/chatapp_web/router.ex
@@ -1,6 +1,8 @@
 defmodule ChatappWeb.Router do
   use ChatappWeb, :router
 
+  import ChatappWeb.UserAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -8,6 +10,7 @@ defmodule ChatappWeb.Router do
     plug :put_root_layout, html: {ChatappWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_scope_for_user
   end
 
   pipeline :api do
@@ -47,5 +50,33 @@ defmodule ChatappWeb.Router do
       live_dashboard "/dashboard", metrics: ChatappWeb.Telemetry
       forward "/mailbox", Plug.Swoosh.MailboxPreview
     end
+  end
+
+  ## Authentication routes
+
+  scope "/", ChatappWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    live_session :require_authenticated_user,
+      on_mount: [{ChatappWeb.UserAuth, :require_authenticated}] do
+      live "/users/settings", UserLive.Settings, :edit
+      live "/users/settings/confirm-email/:token", UserLive.Settings, :confirm_email
+    end
+
+    post "/users/update-password", UserSessionController, :update_password
+  end
+
+  scope "/", ChatappWeb do
+    pipe_through [:browser]
+
+    live_session :current_user,
+      on_mount: [{ChatappWeb.UserAuth, :mount_current_scope}] do
+      live "/users/register", UserLive.Registration, :new
+      live "/users/log-in", UserLive.Login, :new
+      live "/users/log-in/:token", UserLive.Confirmation, :new
+    end
+
+    post "/users/log-in", UserSessionController, :create
+    delete "/users/log-out", UserSessionController, :delete
   end
 end
